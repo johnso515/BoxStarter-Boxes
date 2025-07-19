d96ba2896753fe40c71dfe36a841ee977c3c43b5
John S Sobieski <johnso515@protonmail.com>
2025-07-19 17:27:02 -0500

    feat: Update Utils list

--- 10_DevOps/13_Utilities.ps1
+++ 10_DevOps/13_Utilities.ps1
@@ -2,19 +2,40 @@
 if (!(Get-AppxPackage Microsoft.WindowsTerminal -ErrorAction Ignore)) {
     choco upgrade -y microsoft-windows-terminal
 }
+# Delta provides language syntax-highlighting, within-line insertion/deletion detection, and restructured diff output for git on the command line.
 choco upgrade -y delta
-choco upgrade -y ripgrep
-choco upgrade -y fzf
-choco upgrade -y bat
-choco upgrade -y yq
-choco upgrade -y jq
+
+# A search tool that combines the usability of ag with the raw speed of grep. 
+# choco upgrade -y ripgrep
+
+# fzf is a general-purpose command-line fuzzy finder
+# choco upgrade -y fzf
+
+# Tbd
+# choco upgrade -y bat
+# The aim of the project is to be the jq or sed of yaml files
+# choco upgrade -y yq
+
+# Command-line JSON processor 
+# choco upgrade -y jq
+
+# gh is GitHub on the command line. 
 choco upgrade -y gh
 
+choco upgrade -y chocolateygui
+
+choco upgrade -y bitwarden
+choco upgrade -y bitwarden-cli
+
+# What version do I need
+choco upgrade -y gitversion.portable
+
+choco upgrade -y putty.install
 
 choco upgrade -y git.install --package-parameters="'/GitOnlyOnPath /WindowsTerminal /NoShellIntegration /SChannel'"
 
 # choco upgrade -y gitkraken
-# choco upgrade -y filezilla
+choco upgrade -y filezilla
 # choco upgrade -y cyberduck
 
 ## I tried using terraform for infrastructure as code, but I don't like it
